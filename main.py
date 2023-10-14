8d59a2d86de674f5b373baf1639db51dcfd173b6
MichalTurek <michaltu999@gmail.com>
2023-10-14 13:38:32 +0200

    Modified requirements, added working data builder for flying chairs, modified training function in flow_net

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from core.builders import InputDataBuilder
+from core.builders.flying_chairs_builder import FlyingChairsDataGenerator
+from core.builders.input_data_builder import  InputDataBuilder
 from models.flow_nets import FlowNet
+from tensorflow.python.client import device_lib
 if __name__ == '__main__':
-    input_data_builder = InputDataBuilder().build()
+    # if out of gpu memory error, try smaller batch_size
+    data_generator = FlyingChairsDataGenerator(batch_size=16)
     flow_net = FlowNet()
     flow_net.create_model()
-    #flow_net.model.
+    flow_net.train(data_generator, epochs=10)
--- models/flow_nets.py
+++ models/flow_nets.py
@@ -1,14 +1,22 @@
-import keras
-from keras.layers import Conv2D, Conv2DTranspose, Input, Concatenate
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Input, Concatenate
 from config import MODEL_INPUT_SHAPE, TRAINING
+from datetime import datetime as d
+
+class EndPointError(tf.keras.losses.Loss):
+    def call(self, y_true, y_pred):
+        return tf.keras.backend.sqrt(
+            tf.keras.backend.sum(
+                tf.keras.backend.square(tf.keras.preprocessing.image.smart_resize(y_true, y_pred.shape[1:3]) - y_pred),
+                axis=1, keepdims=True))
 
 
 class FlowNet:
     def __init__(self):
-        self._model: keras.Sequential | None = None
+        self._model = None
 
     @property
-    def model(self) -> keras.Sequential:
+    def model(self) -> tf.keras.Sequential:
         if self._model is None:
             print("Model not present.")
         else:
@@ -29,67 +37,77 @@ class FlowNet:
         conv6 = Conv2D(1024, (3, 3), padding='same', activation='relu', strides=(2, 2))(conv5_1)
         conv6_1 = Conv2D(1024, (3, 3), padding='same', activation='relu')(conv6)
         """ The paper itself doesn't have this documented but all implementations, including the original authors, use an extra flow path in the code. """
-        predict_6 = Conv2D(name='predict_6', filters=2, kernel_size=3, strides=(1, 1), padding='same',
-                                           activation='relu')(conv6_1)
 
-        """ Expanding part """
+        predict_6 = Conv2D(name='predict_6', filters=2, kernel_size=3, strides=(1, 1), padding='same',
+                           activation=None)(conv6_1)
+
         upconv5 = Conv2DTranspose(name='upconv_5', filters=512, kernel_size=(4, 4), strides=(2, 2),
-                                                   padding='same', activation='relu')(conv6)
+                                  padding='same', activation='relu')(conv6)
         flow6 = Conv2DTranspose(name='flow_6', filters=2, kernel_size=(4, 4), strides=(2, 2),
-                                                 padding='same', activation='relu')(predict_6)
+                                padding='same', activation='relu')(predict_6)
         concat5 = Concatenate(name='concat_5', axis=-1)([upconv5, conv5_1, flow6])
         predict5 = Conv2D(name='predict_5', filters=2, kernel_size=3, strides=(1, 1), padding='same',
-                                           activation=None)(concat5)
+                          activation=None)(concat5)
 
         upconv4 = Conv2DTranspose(name='upconv_4', filters=256, kernel_size=(4, 4), strides=(2, 2),
-                                                   padding='same', activation='relu')(concat5)
+                                  padding='same', activation='relu')(concat5)
         flow5 = Conv2DTranspose(name='flow_5', filters=2, kernel_size=(4, 4), strides=(2, 2),
-                                                 padding='same', activation='relu')(predict5)
+                                padding='same', activation='relu')(predict5)
         concat4 = Concatenate(name='concat_4', axis=-1)([upconv4, conv4_1, flow5])
         predict4 = Conv2D(name='predict_4', filters=2, kernel_size=3, strides=(1, 1), padding='same',
-                                           activation=None)(concat4)
+                          activation=None)(concat4)
 
         upconv3 = Conv2DTranspose(name='upconv_3', filters=128, kernel_size=(4, 4), strides=(2, 2),
-                                                   padding='same', activation='relu')(concat4)
+                                  padding='same', activation='relu')(concat4)
         flow4 = Conv2DTranspose(name='flow_4', filters=2, kernel_size=(4, 4), strides=(2, 2),
-                                                 padding='same', activation='relu')(predict4)
+                                padding='same', activation='relu')(predict4)
         concat3 = Concatenate(axis=-1)([upconv3, conv3_1, flow4])
-        predict3 = Conv2D(filters=2, kernel_size=3, strides=(1, 1), padding='same',
-                                           activation=None)(concat3)
+        predict3 = Conv2D(name='predict_3',filters=2, kernel_size=3, strides=(1, 1), padding='same',
+                          activation=None)(concat3)
 
         upconv2 = Conv2DTranspose(filters=64, kernel_size=(4, 4), strides=(2, 2),
-                                                   padding='same', activation='relu')(concat3)
+                                  padding='same', activation='relu')(concat3)
         flow3 = Conv2DTranspose(filters=2, kernel_size=(4, 4), strides=(2, 2),
-                                                 padding='same', activation='relu')(predict3)
+                                padding='same', activation='relu')(predict3)
         concat2 = Concatenate(axis=-1)([upconv2, conv2, flow3])
-        predict2 = Conv2D(filters=2, kernel_size=3, strides=(1, 1), padding='same',
-                                           activation=None)(concat2)
+        predict2 = Conv2D(name='predict_2',filters=2, kernel_size=3, strides=(1, 1), padding='same',
+                          activation=None)(concat2)
 
         upconv1 = Conv2DTranspose(filters=64, kernel_size=(4, 4), strides=(2, 2),
-                                                   padding='same', activation='relu')(concat2)
+                                  padding='same', activation='relu')(concat2)
         flow2 = Conv2DTranspose(filters=2, kernel_size=(4, 4), strides=(2, 2),
-                                                 padding='same', activation='relu')(predict2)
+                                padding='same', activation='relu')(predict2)
         concat1 = Concatenate(axis=-1)([upconv1, conv1, flow2])
-        predict1 = Conv2D(filters=2, kernel_size=3, strides=(1, 1), padding='same',
-                                           activation=None)(concat1)
-
+        predict1 = Conv2D(name='predict_1', filters=2, kernel_size=3, strides=(1, 1), padding='same',
+                          activation=None)(concat1)
         if TRAINING:
-            self._model = keras.Model(
+            self._model = tf.keras.Model(
                 inputs=input_layer,
                 outputs=[predict_6, predict5, predict4, predict3, predict2, predict1]
             )
+        else:
+            self._model = tf.keras.Model(inputs=input_layer, outputs=predict1)
+        epe = EndPointError()
 
-        self._model = keras.Model(inputs=input_layer, outputs=predict1)
-        self.model.compile(loss='epe')
+        self.model.compile(loss=[epe, epe, epe, epe, epe, epe])
         self.model.summary()
 
-    def train(self, x_train, y_train):
+    def train(self, data_generator, epochs=10, steps_per_epoch=None, validation_data=None):
         self.model.fit(
-            x=x_train,
-            y=y_train,
-            epochs=10,
-            shuffle=True,
-            validation_split=0.1,
-            steps_per_epoch=len(x_train),
-            batch_size=64
+            data_generator,
+            epochs=epochs,
+            steps_per_epoch=steps_per_epoch,
+            validation_data=validation_data
         )
+        date_time = d.now().strftime("%m_%d_%Y__%H_%M_%S") + ".keras"
+        self.model.save(date_time)
+    # def train(self, x_train, y_train):
+    #     self.model.fit(
+    #         x=x_train,
+    #         y=y_train,
+    #         epochs=10,
+    #         shuffle=True,
+    #         validation_split=0.1,
+    #         steps_per_epoch=len(x_train),
+    #     )
+

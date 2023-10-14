247973347d80b60823f3e542d98754bd83af5a30
MichalTurek <michaltu999@gmail.com>
2023-10-14 13:39:59 +0200

    Modified requirements, added working data builder for flying chairs, modified training function in flow_net

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-PATH_TO_IMAGES = "C:\\Users\\micha\\Downloads\\FlyingChairs\\FlyingChairs_release\\data\\"
+PATH_TO_IMAGES = "FlyingChairs/FlyingChairs_release/data/"
 NUMBER_OF_SAMPLES_TO_LOAD = 5008
 TRAINING = True
 MODEL_INPUT_SHAPE = (384, 512, 6)
--- core/builders/input_data_builder.py
+++ core/builders/input_data_builder.py
@@ -9,11 +9,9 @@ from config import PATH_TO_IMAGES, NUMBER_OF_SAMPLES_TO_LOAD
 class InputDataBuilder:
     def __init__(self):
         self.images = []
-
+        self.flows = []
     def build(self):
-        flows, images = self._load_data()
-        print(flows.shape)
-        print(images.shape)
+        self.flows, self.images = self._load_data()
         return self
 
     def _split_train_test(self):
@@ -25,9 +23,9 @@ class InputDataBuilder:
         flows = []
         images = []
         for i in range(0, NUMBER_OF_SAMPLES_TO_LOAD, 3):
-            print(files_names[i])
             with open(f"{PATH_TO_IMAGES}/{files_names[i]}", 'rb') as f:
                 header = f.read(4)
+                print(header)
                 if header.decode("utf-8") != 'PIEH':
                     raise Exception('Flow file header does not contain PIEH')
 
@@ -38,12 +36,11 @@ class InputDataBuilder:
 
             flows.append(data)
             images.append(
-                np.expand_dims(
                     np.concatenate([
                         Image.open(f"{PATH_TO_IMAGES}/{files_names[i+1]}"),
                         Image.open(f"{PATH_TO_IMAGES}/{files_names[i+2]}")
                     ], axis=-1
-                    ), axis=0)
+                    )
             )
 
         return np.array(flows), np.array(images)
